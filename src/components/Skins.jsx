8cae3fae502fd6f39f481e52b7e4c226b0a64bd0
Jörg <jbantin@gmx.de>
2024-12-02 10:36:40 +0100

    hotfix5

--- src/components/Skins.jsx
+++ src/components/Skins.jsx
@@ -15,7 +15,7 @@ const mapArray = [
   {
     name: "Aqua",
     attribution:
-      '&copy; <a href="https://www.stadiamaps.com/" >Stadia Maps</a> &copy; <a href="https://www.stamen.com/" >Stamen Design</a> &copy; <a href="https://openmaptiles.org/" >OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      '&copy;  <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     url: "https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg",
   },
   {
