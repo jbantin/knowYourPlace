e31c828a522e7b4a28a2584a2da58eb8fa316b8f
Jörg <jbantin@gmx.de>
2024-12-02 14:44:00 +0100

    clouds

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -57,6 +57,12 @@ const Map = () => {
   const railRoadHandler = (e) => {
     setRailroads(railRoadsCheckbox.current.checked);
   };
+  const [clouds, setClouds] = useState(false);
+  let cloudCheckbox = useRef();
+
+  const cloudHandler = (e) => {
+    setClouds(cloudCheckbox.current.checked);
+  };
 
   const contextData = useContext(LocationContext);
   const { map, setMap, zoom } = contextData;
@@ -77,6 +83,14 @@ const Map = () => {
             attribution={mapArray[map].attribution}
             url={mapArray[map].url}
           />
+          {clouds ? (
+            <TileLayer
+              attribution='Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>'
+              url="http://{s}.tile.openweathermap.org/map/clouds/{z}/{x}/{y}.png?appid=de069a928e6b04d0efc907091fbbae01&units=metric"
+            />
+          ) : (
+            <></>
+          )}
           {railRoads ? (
             <TileLayer
               attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Map style: &copy; <a href="https://www.OpenRailwayMap.org">OpenRailwayMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
@@ -94,14 +108,21 @@ const Map = () => {
           />
           <LocationMarker />
         </MapContainer>
-        <p className="absolute z-[1600] top-[30%] left-[4vw] xl:left-[2vw] ">
-          <input
-            type="checkbox"
-            ref={railRoadsCheckbox}
-            onClick={railRoadHandler}
-          />{" "}
-          Railroads
-        </p>
+        <div className="absolute z-[1600] top-[30%] left-[4vw] xl:left-[2vw] bg-slate-200 bg-opacity-60 rounded-xl p-2">
+          {" "}
+          <p className="">
+            <input
+              type="checkbox"
+              ref={railRoadsCheckbox}
+              onClick={railRoadHandler}
+            />{" "}
+            Railroads
+          </p>
+          <p>
+            <input type="checkbox" ref={cloudCheckbox} onClick={cloudHandler} />{" "}
+            Clouds
+          </p>
+        </div>
       </div>
     </>
   );
