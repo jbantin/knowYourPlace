f113670d7e676e5fbf8a3b28361a1158ab4de4e2
Jörg <jbantin@gmx.de>
2024-12-07 22:18:01 +0100

    test

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -110,7 +110,7 @@ const Map = () => {
           {names ? (
             <TileLayer
               attribution='Map data: &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://tiles.stadiamaps.com/tiles/stamen_toner_labels/{z}/{x}/{y}{r}.png"
+              url="https://tiles.stadiamaps.com/tiles/stamen_terrain_labels/{z}/{x}/{y}{r}.png"
             />
           ) : (
             <></>
